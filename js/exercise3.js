7e59efde1bf1fa1c0f8bf7a2293183f216d440ad
Diana Laura Gonzalez Escamilla <dianathcb@gmail.com>
2025-09-17 01:27:13 -0700

    exercise3 completed

---
+++ js/exercise3.js
@@ -0,0 +1,17 @@
+const a = 1
+const b = 2
+const c = 3
+
+if (a>b) {
+    if (a>c) {
+        console.log(a)
+    } else {
+        console.log(c)
+} 
+} else {
+     if (b>c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
+}
