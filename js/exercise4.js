edc2fa4b28e01777085b4ae8b140bee908e6054c
Diana Laura Gonzalez Escamilla <dianathcb@gmail.com>
2025-09-17 02:21:08 -0700

    refactor: simplify triangle validation logic and remove redundant checks

--- js/exercise4.js
+++ js/exercise4.js
@@ -1,20 +1,19 @@
 const a = 10
 const b = 7
-const c = 5
-
+const c = 1
+if (a==0) {
+    if (b==0) {
+        if (c==0) {
+            console.log("Not possible")
+        }
+    }
+}
 if (a+b>c) {
     if (a+c>b) {
         if (b+c>a) {
             console.log("Possible Triangle")
         } else {
             console.log("Not Triangle")
-            if (a===0) {
-                if (b===0) {
-                    if (c===0) {
-                        console.log("Not possible")
-                    }
-                }
-            }
         }
     }
 }
